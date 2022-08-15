a62b13456669a5e49d7997594994f730148fdfab
Replit user <>
2022-08-15 01:17:16 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -77,9 +77,9 @@ typedef struct {
    int capacidad; // capacidad del arreglo
 } Vector;
 
-Vector  crearVector(int n) {
+Vector * crearVector(int n) {
    Vector* v = (Vector)malloc(sizeof(Vector));
-  v->datos=(int) malloc (sizeof(int)*n);
+  v->datos=(int*) malloc (sizeof(int)*n);
   v->capacidad=n;
    return NULL;
 }
